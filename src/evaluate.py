0418fead4b7da77733e63d755d26b581d78628a6
shikharbhardwaj <shikharbhardwaj68@gmail.com>
2018-12-06 23:54:03 +0000

    Implement DQN agent according to FinGame

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,59 +1,49 @@
-#!/usr/bin/env python3
+import keras
+from keras.models import load_model
 
-from agent import Agent
-from btgym import BTgymEnv
-from gym import spaces
-import numpy as np
+from agent.agent import Agent
+from functions import *
+import sys
 
-import matplotlib.pyplot as plt
+if len(sys.argv) != 3:
+	print "Usage: python evaluate.py [stock] [model]"
+	exit()
 
-# Path to the saved model
-model_path = './model100'
+stock_name, model_name = sys.argv[1], sys.argv[2]
+model = load_model("models/" + model_name)
+window_size = model.layers[0].input.shape.as_list()[1]
 
-# Path to the data being tested
-data_path = '/home/shikhar/dev/btgym/examples/data/DAT_ASCII_EURUSD_M1_2016.csv'
+agent = Agent(window_size, True, model_name)
+data = getStockDataVec(stock_name)
+l = len(data) - 1
+batch_size = 32
 
-# Evaluation environment parameters
-cash = 10000
-state_len = 32
+state = getState(data, 0, window_size + 1)
+total_profit = 0
+agent.inventory = []
 
-env     = BTgymEnv(filename=data_path,
-                   state_shape={'raw': spaces.Box(low=0.8, high=2,shape=(state_len, 4))},
-                   skip_frame=5,
-                   start_cash=cash,
-                   broker_commission=0.02,
-                   fixed_stake=100,
-                   drawdown_call=90,
-                   render_ylabel='Price Lines',
-                   render_size_episode=(12,8),
-                   render_size_human=(8, 3.5),
-                   render_size_state=(10, 3.5),
-                   render_dpi=75,
-                   verbose=0,) 
+for t in xrange(l):
+	action = agent.act(state)
 
-print(env.strategy)
-ag = Agent(env)
-ag.load_model(model_path)
+	# sit
+	next_state = getState(data, t + 1, window_size + 1)
+	reward = 0
 
-trial_len = 500
-cur_state = list(env.reset().items())[0][1]
-cur_state = np.reshape(cur_state, (1, state_len, 4))
+	if action == 1: # buy
+		agent.inventory.append(data[t])
+		print "Buy: " + formatPrice(data[t])
 
-portfolio_history = []
+	elif action == 2 and len(agent.inventory) > 0: # sell
+		bought_price = agent.inventory.pop(0)
+		reward = max(data[t] - bought_price, 0)
+		total_profit += data[t] - bought_price
+		print "Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price)
 
-print('Intial portfolio worth = ', cash)
-for i in range(trial_len):
-    action = ag.get_action(cur_state, mode='eval')
-    new_state, reward, done, info = env.step(action)
-    new_state = list(new_state.items())[0][1]
-    new_state = np.reshape(new_state, (1, state_len, 4))
-    cur_state = new_state
-    portfolio_history.append(info[0]['broker_value'])
-    print("Agent took action", action)
-    print("Broker value ", portfolio_history[-1])
+	done = True if t == l - 1 else False
+	agent.memory.append((state, action, reward, next_state, done))
+	state = next_state
 
-# plt.plot(np.linspace(0, trial_len - 1, trial_len), portfolio_history)
-# plt.show()
-print('Final portfolio worth = ', portfolio_history[-1])
-
-env.close()
+	if done:
+		print "--------------------------------"
+		print stock_name + " Total Profit: " + formatPrice(total_profit)
+		print "--------------------------------"
--- src/train.py
+++ src/train.py
@@ -1,73 +1,97 @@
-#!/usr/bin/env python3
+from agent.agent import Agent
+from functions import *
+import sys
 
-from agent import Agent
-from btgym import BTgymEnv
-from gym import spaces
-import numpy as np
-import math
+if len(sys.argv) != 4:
+        print("Usage: python train.py [stock] [window] [episodes]")
+        exit()
 
-# Path to the data being tested
-data_path = '/home/shikhar/dev/btgym/examples/data/DAT_ASCII_EURUSD_M1_2016.csv'
+stock_name, window_size, episode_count = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
 
-# Training environment parameters
-cash = 100000
-state_len = 128
+agent = Agent(window_size)
 
-env     = BTgymEnv(filename=data_path,
-                   state_shape={'raw': spaces.Box(low=0.8, high=2, shape=(state_len, 4))},
-                #    skip_frame=5,
-                   start_cash=cash,
-                #    broker_commission=0.001,
-                #    fixed_stake=1,
-                #    drawdown_call=90,
-                   render_ylabel='Price Lines',
-                   render_size_episode=(12,8),
-                   render_size_human=(8, 3.5),
-                   render_size_state=(10, 3.5),
-                   render_dpi=75,
-                   verbose=0,) 
+data = getStockDataVec(stock_name)
+l = data.shape[0] - 1
+batch_size = 400
+state_dim = 22
 
-episodes = 101
-trial_len = 200
 
-agent = Agent(env=env)
+for e in range(episode_count + 1):
+        print("Episode " + str(e) + "/" + str(episode_count))
 
-# State shape is (batch_size, timesteps, dim)
-# https://stackoverflow.com/questions/43234504/understanding-lstm-input-shape-in-keras-with-different-sequence
-# https://stats.stackexchange.com/questions/274478/understanding-input-shape-parameter-in-lstm-with-keras
+        # Get inital state
+        action = 0
+        initial_state = np.zeros((window_size + 1, state_dim))
+        initial_state[:, 0:(state_dim - 3)] = data[0:window_size + 1]
+        state = getState(data, 0, window_size + 1, action, initial_state)
 
-for e in range(episodes): 
-    cur_state = np.array(list(env.reset().items())[0][1])
-    cur_state = np.reshape(cur_state, (1, state_len, 4))
-    score = 0
-    max_positive_reward = 0
+        cash = 10000
+        trade_size = 1000
+        prev_portfolio_value = cash
+        total_profit = 0
+        agent.inventory = []
 
-    for step in range(trial_len):
-        action = agent.get_action(cur_state)
-        new_state, reward, done, info = env.step(action)
-        # print('ACTION: {}\nREWARD: {}\nINFO: {}'.format(action, reward, info))
+        for t in range(window_size, l):
+                prev_price = state[-1][0]
+                action = agent.act(state)
 
-        reward = reward * 10000000  if not done else -10
+                # Get next state.
+                next_state = getState(data, t + 1, window_size + 1, action, state)
+                cur_price = next_state[-1][0]
+                reward = 0
 
-        max_positive_reward = max(max_positive_reward, reward)
+                # The reward is
+                # If we sell,
+                portfolio_value_sale = cash + len(agent.inventory) * cur_price
 
-        new_state = list(new_state.items())[0][1]
-        new_state = np.reshape(new_state, (1, state_len, 4))
-        action_idx = agent.encode_action(action)
+                if agent.inventory != []:
+                    portfolio_value_sale += trade_size * (cur_price - agent.inventory[0])
 
-        agent.remember(cur_state, action_idx, reward, new_state, done)
-        agent.train_model()            # internally iterates default (prediction) model
+                sell_reward = np.log(portfolio_value_sale / prev_portfolio_value)
 
-        score += reward
-        cur_state = new_state
-        if done:
-            break
-    
-    agent.update_target_model()
-    print("Episode :", e, ", score :", score, "memory length : ", len(agent.memory), "epsilon :", agent.epsilon, 'max pos reward : ', max_positive_reward)
+                # If we buy,
+                portfolio_value_buy = cash + len(agent.inventory) * cur_price
+                buy_reward = np.log(portfolio_value_buy / prev_portfolio_value)
 
-    # dqn_agent.render_all_modes(env)
-    if e % 20 == 0:
-        agent.save_model('model{}'.format(e))
-    
-env.close()
+                # If we hold,
+                portfolio_value_hold = cash + len(agent.inventory) * cur_price
+                hold_reward = np.log(portfolio_value_hold / prev_portfolio_value)
+
+                prev_portfolio_value = portfolio_value_hold
+                reward = hold_reward
+
+                if action == 1 and cash >= cur_price: # buy
+                        agent.inventory.append(cur_price)
+                        print("Buy: " + formatPrice(cur_price))
+                        prev_portfolio_value = portfolio_value_buy
+                        reward = buy_reward
+                        cash -= cur_price
+                elif action == 2 and len(agent.inventory) > 0: # sell
+                        bought_price = agent.inventory.pop(0)
+                        total_profit += (cur_price - bought_price) * trade_size
+                        print("Sell: " + formatPrice(cur_price) + " | Profit: "
+                              + formatPrice(cur_price - bought_price))
+                        prev_portfolio_value = portfolio_value_sale
+                        cash += cur_price
+                        reward = sell_reward
+
+                done = True if t == l - 1 else False
+                agent.memory.append((state, 0, hold_reward, next_state, done))
+                agent.memory.append((state, 1, buy_reward, next_state, done))
+                agent.memory.append((state, 2, sell_reward, next_state, done))
+                state = next_state
+
+                if done:
+                        print("--------------------------------")
+                        print("Total Profit: " + formatPrice(total_profit))
+                        print("Agent params:")
+                        print("ε :", agent.epsilon)
+                        print("--------------------------------")
+
+                if len(agent.memory) > batch_size:
+                        agent.expReplay(batch_size)
+
+                agent.targetUpdate()
+
+        if e % 10 == 0:
+                agent.model.save("models/lstm/model_ep" + str(e))
