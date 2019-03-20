0395352efd2785ce141375e062f0a499603f7f5a
shikharbhardwaj <shikharbhardwaj68@gmail.com>
2019-03-20 21:32:29 +0530

    Action metrics export during evalutaion.

--- src/test.py
+++ src/test.py
@@ -3,6 +3,9 @@
 
 import json
 import sys
+import time
+
+import pandas as pd
 
 from environment.data import BatchIterator
 from environment.portfolio import Portfolio
@@ -23,6 +26,9 @@ trade_pair = config['trade_pair']
 start_cash = config['start_cash']
 trade_size = config['trade_size']
 
+# Output metrics
+action_series_location = "metrics/actions_" + str(int(time.time())) + ".csv"
+
 # Data iterators
 state_iter = BatchIterator(location, pairs, begin_year, end_year)
 price_iter = BatchIterator(location, [trade_pair], begin_year, end_year, False)
@@ -32,20 +38,23 @@ portfolio = Portfolio(start_cash, trade_size, price_iter)
 env = Environment(pairs, state_iter, portfolio)
 
 state_shape = env.state().shape
-print(state_shape)
 
 agent = Agent(state_shape[0], is_eval = True, model_location = model_location)
 
 num_steps = 1
 
+actions = pd.DataFrame(columns=['tick', 'action'])
+
 while True:
+    tick = env.current_tick[0]
     cur_state = env.state()
     action = agent.act(cur_state)
 
     try:
         reward = env.execute(action)
+        actions = actions.append({'tick': tick, 'action': action}, ignore_index=True)
     except StopIteration:
-        print("Training ended after processing", num_steps, "ticks")
+        print("Training ended after processing", num_steps - 1, "ticks")
         print(str(env))
         print(str(agent))
         print()
@@ -61,3 +70,5 @@ while True:
         print()
 
     num_steps += 1
+
+actions.to_csv(action_series_location)
